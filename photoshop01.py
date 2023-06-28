70bb7b60a0f92b6a1870fad76cd844aceee62fc5
totalnosamlud <134202989+totalnosamlud@users.noreply.github.com>
2023-06-28 14:07:48 +0200

    Add files via upload

--- photoshop01.py
+++ photoshop01.py
@@ -7,6 +7,7 @@ import os
 #NIJE RELEVANTNO ZA PREDAVANJE
 
 fotografija_putanja = r"slika.jpg"
+#fotografija_putanja = r"slika2.jpg"
 fotografije_varijabla = Image.open(fotografija_putanja)
 
 red = Image.open(r"red.jpg")
@@ -28,7 +29,8 @@ yellow_vert = yellow.resize((fotografije_varijabla.size[0] - (yellow_resize_vert
 yellow_resize_horizontal=int(round(fotografije_varijabla.size[1]*0.125,0))
 yellow_hor = yellow.resize((10, fotografije_varijabla.size[1] - (yellow_resize_horizontal * 2)))
 
-odabir=int(input('Unesite željeni postotak za micanje ruba slike (cijeli broj bez %): '))
+odabir=int(50)
+#odabir=int(input('Unesite željeni postotak za micanje ruba slike (cijeli broj bez %): '))
 odabir_step= odabir/200
 
 blue_resize_vertical=int(round(fotografije_varijabla.size[0]*odabir_step,0))
@@ -56,19 +58,19 @@ print(fotografije_varijabla.size[0])
 
 fotografije_varijabla.paste(red_vert,(red_resize_vertical,red_resize_horizontal))
 fotografije_varijabla.paste(red_hor,(red_resize_vertical,red_resize_horizontal))
-fotografije_varijabla.paste(red_vert,(red_resize_vertical,fotografije_varijabla.size[1] - red_resize_horizontal))
+fotografije_varijabla.paste(red_vert,(red_resize_vertical,fotografije_varijabla.size[1] - red_resize_horizontal - 10))
 fotografije_varijabla.paste(red_hor,(fotografije_varijabla.size[0] - red_resize_vertical,red_resize_horizontal))
 fotografije_varijabla.paste(p10,(red_resize_vertical,red_resize_horizontal))
 
 fotografije_varijabla.paste(yellow_vert,(yellow_resize_vertical,yellow_resize_horizontal))
 fotografije_varijabla.paste(yellow_hor,(yellow_resize_vertical,yellow_resize_horizontal))
-fotografije_varijabla.paste(yellow_vert,(yellow_resize_vertical,fotografije_varijabla.size[1] - yellow_resize_horizontal))
+fotografije_varijabla.paste(yellow_vert,(yellow_resize_vertical,fotografije_varijabla.size[1] - yellow_resize_horizontal - 10))
 fotografije_varijabla.paste(yellow_hor,(fotografije_varijabla.size[0] - yellow_resize_vertical,yellow_resize_horizontal))
 fotografije_varijabla.paste(p25,(yellow_resize_vertical,yellow_resize_horizontal))
 
 fotografije_varijabla.paste(blue_vert,(blue_resize_vertical,blue_resize_horizontal))
 fotografije_varijabla.paste(blue_hor,(blue_resize_vertical,blue_resize_horizontal))
-fotografije_varijabla.paste(blue_vert,(blue_resize_vertical,fotografije_varijabla.size[1] - blue_resize_horizontal))
+fotografije_varijabla.paste(blue_vert,(blue_resize_vertical,fotografije_varijabla.size[1] - blue_resize_horizontal - 10))
 fotografije_varijabla.paste(blue_hor,(fotografije_varijabla.size[0] - blue_resize_vertical,blue_resize_horizontal))
 fotografije_varijabla.paste(odabrano,(blue_resize_vertical,blue_resize_horizontal))
 
